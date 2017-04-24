9f0a48191befd48065d7b7a1688588cbd484f21a
moranshm0111 <moranshm0111@gmail.com>
2017-04-25 01:14:45 +0300

    working bot

--- Playground/DudisPlayground/test1.py
+++
@@ -1,6 +0,0 @@
-
-from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform("testdoc.doc".__doc__)
-X_train_counts.shape
-count_vect.vocabulary_.get(u'algorithm')
