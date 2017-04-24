caafd5f4dbf437edce0741c5161b44345d3f9dcc
Nimrod Dickler <nimrodd@traiana.com>
2017-04-24 22:06:26 +0300

    with basic Classifier

--- Playground/DudisPlayground/test1.py
+++ Playground/DudisPlayground/test1.py
@@ -1,6 +1,55 @@
 
 from sklearn.feature_extraction.text import CountVectorizer
+tags = [
+  "dudi moran muli nimi","moran","muli"
+]
+
+dataset = [
+    "I don't have internet",
+    "the light is broken",
+    "My paycheck is wrong",
+    "my computer didn't start",
+    "my phone doesn't work",
+    "the table is broken",
+    "the chair is not working",
+    "my keyboard is missing a key",
+    "I didn't get my salary this month",
+    "where can I find the expense report",
+    "how much an i changed for my car",
+    "what is my yearly budget",
+    "I can't open excel"
+]
+target = [
+    "IT",
+    "Maintenance",
+    "Finance",
+    "IT",
+    "IT",
+    "Maintenance",
+    "Maintenance",
+    "IT",
+    "Finance",
+    "Finance",
+    "Finance",
+    "Finance",
+    "IT"
+]
 count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform("testdoc.doc".__doc__)
+X_train_counts = count_vect.fit_transform(dataset).toarray()
 X_train_counts.shape
-count_vect.vocabulary_.get(u'algorithm')
+print (X_train_counts)
+
+dataset1 = [
+    "excel"
+]
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(X_train_counts, target)
+X_new_counts = count_vect.transform(dataset1)
+tfidf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+predicted = clf.predict(X_new_tfidf)
+
+for doc, category in zip(dataset1, predicted):
+    print((doc,category))
