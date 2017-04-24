13a7dcc98bffb873b44ebd867748a190879da3e2
moranshm0111 <moranshm0111@gmail.com>
2017-04-24 20:27:19 +0300

    add test1 + new folder

--- Playground/DudisPlayground/test1.py
+++ Playground/DudisPlayground/test1.py
@@ -1,1 +1,6 @@
-fthfhfgh
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform("testdoc.doc".__doc__)
+X_train_counts.shape
+count_vect.vocabulary_.get(u'algorithm')
