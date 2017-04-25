46bfafa83c093e721e1fbadc12bec3eadef8e9bf
moranshm0111 <moranshm0111@gmail.com>
2017-04-25 20:09:08 +0300

    with translate

--- Playground/DudisPlayground/ReadFromCSV.py
+++ Playground/DudisPlayground/ReadFromCSV.py
@@ -1,5 +1,10 @@
 import csv
-with open('names.csv') as csvfile:
+from mtranslate import translate
+
+with open("C:\Users\dudia\Desktop\TCB\HarelDataSet\HarelDataCRM.csv") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['first_name'], row['last_name'])
+        title = translate(row['Title'].decode('windows-1255').encode('UTF-8'))
+        description = translate(row['Description'].decode('windows-1255').encode('UTF-8'))
+        department = translate(row['Department'].decode('windows-1255').encode('UTF-8'))
+        print(title,description,department)
