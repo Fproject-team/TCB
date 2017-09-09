9c6b59594d8246cabe7ec2bdd59e6f617f4170e6
moranshm0111 <moranshm0111@gmail.com>
2017-09-09 16:34:51 +0300

    file organize

--- Playground/DudisPlayground/mtranslate/receive.py
+++ Playground/DudisPlayground/mtranslate/receive.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, Response
+import sys
+sys.path.append("./Playground/DudisPlayground/mtranslate/Classify/classifyTest")
 from Playground.DudisPlayground.mtranslate.Classify.classifyTest import classify
 # from classifyTestDBOrganize import classify
 from Playground.DudisPlayground.mtranslate.Slack.app import send_to_channel
