df2efad42c8863f5e5a414438372ec00dce69c39
moranshm0111 <moranshm0111@gmail.com>
2017-09-09 19:58:11 +0300

    file organize

--- Playground/DudisPlayground/mtranslate/Classify/DB/DBOrganize.py
+++ Playground/DudisPlayground/mtranslate/Classify/DB/DBOrganize.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
 import pymysql
-def WriteToDB(ticket_data,ticket_department):
+def WriteToDB(ticket_data,ticket_department,comp_name):
     data = ticket_data
     department = ticket_department
+    comp = comp_name
     connection = pymysql.connect(host='104.199.79.238', port=3306, user='root', passwd='123456', db='test')
     cun = connection.cursor()
     # Create a new record
     sql = "INSERT INTO `DBOrganize` (`Data`,`Department`,`comp_name`) VALUES (%s,%s,%s)"
-    cun.execute(sql, (data,department,'Harel'))
+    cun.execute(sql, (data,department,comp))
     # connection is not autocommit by default. So you must commit to save
     # your changes.
     connection.commit()
